de27b07f3b69c91171c4f0147457e3f13c487a45
ernesto1616 <ernestospam17@gmail.com>
2026-07-08 21:23:20 -0400

    Clean up comments in CompensationVersionExport
    Removed comments for clarity in CompensationVersionExport struct.

--- Model/CompensationVersionExport.cs
+++ Model/CompensationVersionExport.cs
@@ -6,10 +6,7 @@ namespace CompensationExportLibrary.Models
     [OSStructure(Description = "One compensation version = one folder in the ZIP")]
     public struct CompensationVersionExport
     {
-        // Used to name the folder inside the ZIP
         [OSStructureField] public string SalaryScaleModelName { get; set; }
-
-        // The full SQL output (all grade rows) for this version
         [OSStructureField] public List<CompensationRow> Rows { get; set; }
     }
 }
